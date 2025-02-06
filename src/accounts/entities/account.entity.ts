95b1a5caf48592cb103250da4b18d348ddc2b7ab
kodaimura <kodaimura123@gmail.com>
2025-02-06 23:19:12 +0900

    install typeorm

--- src/accounts/entities/account.entity.ts
+++ src/accounts/entities/account.entity.ts
@@ -1,1 +1,19 @@
-export class Account {}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('accounts')
+export class Account {
+  @PrimaryGeneratedColumn()
+  account_id: number;
+
+  @Column({ unique: true, length: 150 })
+  account_name: string;
+
+  @Column()
+  account_password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
+import { Account } from './accounts/entities/account.entity';
 
 @Module({
-  imports: [AccountsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [Account],
+      synchronize: true,
+    }),
+    AccountsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
